2f44adf386f0d3176052b114c1507eec89d9ee36
srgts <srgts1@mail.ru>
2017-07-17 16:50:43 +0300

    ?!

--- first/src/main/java/ThreadTest.java
+++ first/src/main/java/ThreadTest.java
@@ -1,7 +1,41 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Sergey.Tsaturov on 29.06.2017.
  */
-public class q {
+public class ThreadTest implements Runnable {
+    final static private Object shared=new Object();
+    private int type;
+    public ThreadTest(int i) {
+        type=i;
+    }
+    public void run() {
+        if (type==1 || type==2) {
+            synchronized (shared) {
+                try {
+                    shared.wait();
+                } catch (InterruptedException e) {}
+                System.out.println("Thread "+type+
+                        " after wait()");
+            }
+        } else {
+            synchronized (shared) {
+                shared.notifyAll();
+                System.out.println("Thread "+type+
+                        " after notifyAll()");
+            }
+        }
+    }
+    public static void main(String s[]) {
+        ThreadTest w1 = new ThreadTest(1);
+        new Thread(w1).start();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {}
+        ThreadTest w2 = new ThreadTest(2);
+        new Thread(w2).start();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {}
+        ThreadTest w3 = new ThreadTest(3);
+        new Thread(w3).start();
+    }
 }
--- first/src/main/java/WaitThread.java
+++ first/src/main/java/WaitThread.java
@@ -1,7 +1,27 @@
-package PACKAGE_NAME;
-
 /**
  * Created by Sergey.Tsaturov on 29.06.2017.
  */
-public class WaitThread {
-}
+public class WaitThread implements Runnable {
+    private Object shared;
+    public WaitThread(Object o) {
+        shared=o;
+    }
+    public void run() {
+        synchronized (shared) {
+            try {
+                shared.wait();
+            } catch (InterruptedException e) {}
+            System.out.println("after wait");
+        }
+    }
+    public static void main(String s[]) {
+        Object o = new Object();
+        WaitThread w = new WaitThread(o);
+        new Thread(w).start();
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {} System.out.println("before notify");
+        synchronized (o) {
+            o.notifyAll();
+        }
+    }}
